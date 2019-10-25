c8998a4993b20b2927fd2e13dc50e1010301df9a
tlahmann <tobiaslahmann@gmail.com>
2019-10-25 15:52:56 +0200

    Add study results tables

--- appendices/survey_eval.r
+++ appendices/survey_eval.r
@@ -44,14 +44,31 @@ names(df_results)[names(df_results) == "Please.enter.your.age.in.years"] <- "age
 names(df_results)[names(df_results) == "Please.select.your.gender"] <- "gender"
 names(df_results)[names(df_results) == "What.subject..if.any..did.you.study.or.are.you.currently.studying."] <- "Current studies"
 names(df_results)[names(df_results) == "What.is.the.highest.degree.or.level.of.school.you.have.completed.Â.If.currently.enrolled..highest.degree.received."] <- "highestDegree"
-names(df_results)[names(df_results) == "How.much.experience.do.you.have.with.virtual.reality..VR..and.augmented.reality..AR....VR."] <- "experience(VR)"
-names(df_results)[names(df_results) == "How.much.experience.do.you.have.with.virtual.reality..VR..and.augmented.reality..AR....AR."] <- "experience(AR)"
+names(df_results)[names(df_results) == "How.much.experience.do.you.have.with.virtual.reality..VR..and.augmented.reality..AR....VR."] <- "experienceVR"
+names(df_results)[names(df_results) == "How.much.experience.do.you.have.with.virtual.reality..VR..and.augmented.reality..AR....AR."] <- "experienceAR"
 
 # Change data selection
 df_results$gender <- as.character(df_results$gender)
 df_results$gender[df_results$gender == "A1"] <- "female"
 df_results$gender[df_results$gender == "A2"] <- "male"
 df_results$gender[df_results$gender == "A3"] <- "diverse"
+
+df_results$experienceVR <- as.character(df_results$experienceVR)
+df_results$experienceVR[df_results$experienceVR == "A1"] <- "[A1] No experience at all"
+df_results$experienceVR[df_results$experienceVR == "A2"] <- "[A2] Almost no experience"
+df_results$experienceVR[df_results$experienceVR == "A3"] <- "[A3] Less than average experience"
+df_results$experienceVR[df_results$experienceVR == "A4"] <- "[A4] Some experience"
+df_results$experienceVR[df_results$experienceVR == "A5"] <- "[A5] More than average experience"
+df_results$experienceVR[df_results$experienceVR == "A6"] <- "[A6] Experienced"
+df_results$experienceVR[df_results$experienceVR == "A7"] <- "[A7] Very highly experienced"
+df_results$experienceAR <- as.character(df_results$experienceAR)
+df_results$experienceAR[df_results$experienceAR == "A1"] <- "[A1] No experience at all"
+df_results$experienceAR[df_results$experienceAR == "A2"] <- "[A2] Almost no experience"
+df_results$experienceAR[df_results$experienceAR == "A3"] <- "[A3] Less than average experience"
+df_results$experienceAR[df_results$experienceAR == "A4"] <- "[A4] Some experience"
+df_results$experienceAR[df_results$experienceAR == "A5"] <- "[A5] More than average experience"
+df_results$experienceAR[df_results$experienceAR == "A6"] <- "[A6] Experienced"
+df_results$experienceAR[df_results$experienceAR == "A7"] <- "[A7] Very highly experienced"
 
 # print to console the results of ls() call:
 print("Data objects:")
@@ -79,6 +96,7 @@ library(tidyr)
 library(matrixStats)
 
 print(table(df_results["gender"]))
+print(stat.desc(df_results["age"]))
 
 ##################################
 ##################################
@@ -116,14 +134,15 @@ if (plot) {
   pie +
       geom_text(aes(y = value / 3 + c(0, cumsum(value)[-length(value)]),
                       label = percent(value / length(df_results$gender))), size = 6) +
-    scale_fill_manual(values = c("#941c20", "#009269", "#4A5745"))
+    scale_fill_manual(values = c("#d73027", "#4575b4", "#ffffbf"))
 
 
   ### Plot VR experience
-  t <- table(df_results["experience(VR)"])
-  g <- c("A1", "A2", "A3", "A4", "A6", "A7")
-  v <- c(t[g[1]], t[g[2]], t[g[3]], t[g[4]], t[g[5]], t[g[6]])
+  t <- table(df_results["experienceVR"])
+  g <- c("[A1] No experience at all", "[A2] Almost no experience", "[A3] Less than average experience", "[A4] Some experience", "[A5] More than average experience", "[A6] Experienced", "[A7] Very highly experienced")
+  v <- c(t[g[1]], t[g[2]], t[g[3]], t[g[4]], t[g[5]], t[g[6]], t[g[7]])
   v[is.na(v)] <- 0
+  v <- as.numeric(as.character(v))
   df_plot <- data.frame(
     group = g,
     value = v
@@ -134,9 +153,26 @@ if (plot) {
     blank_theme +
     labs(x = "", y = "") +
     guides(fill = guide_legend(title = "Experience (VR)")) +
-    theme(axis.text.x=element_blank()) +
-      geom_text(aes(y = value / 3 + c(0, cumsum(value)[-length(value)]), 
-                label = percent(value/100)), size=5)
+    theme(axis.text.x=element_blank())
+  pie + scale_fill_manual(values = c("#d73027", "#fc8d59", "#fee090", "#ffffbf", "#e0f3f8", "#91bfdb", "#4575b4"))
+
+  ### Plot AR experience
+  t <- table(df_results["experienceAR"])
+  g <- c("[A1] No experience at all", "[A2] Almost no experience", "[A3] Less than average experience", "[A4] Some experience", "[A5] More than average experience", "[A6] Experienced", "[A7] Very highly experienced")
+  v <- c(t[g[1]], t[g[2]], t[g[3]], t[g[4]], t[g[5]], t[g[6]], t[g[7]])
+  v[is.na(v)] <- 0
+  v <- as.numeric(as.character(v))
+  df_plot <- data.frame(
+    group = g,
+    value = v
+  )
+  bp <- ggplot(df_plot, aes(x = "", y = value, fill = group)) +
+    geom_bar(width = 1, stat = "identity")
+  pie <- bp + coord_polar("y", start = 0) +
+    blank_theme +
+    labs(x = "", y = "") +
+    guides(fill = guide_legend(title = "Experience (AR)")) +
+    theme(axis.text.x=element_blank()) 
   pie + scale_fill_manual(values = c("#d73027", "#fc8d59", "#fee090", "#ffffbf", "#e0f3f8", "#91bfdb", "#4575b4"))
 
   # # "Collection Count", "Sam Pleasure", "Sam Arousal", "Sam Dominance"
