4d0f56d35f38494f5a186c10142fd1c9166439e8
tlahmann <tobiaslahmann@gmail.com>
2019-10-28 17:27:22 +0100

    Add more stats visualizations

--- appendices/survey_eval.r
+++ appendices/survey_eval.r
@@ -14,7 +14,7 @@ file <- c("results-survey512856.csv", "results-survey382784.csv", "results-surve
 
 # read in the csv file:
 df_results <-
-  read.csv(file[[3]], header = TRUE) 	# read in the first line as column labels with header=TRUE
+  read.csv(file[[3]], header = TRUE, stringsAsFactors = FALSE) 	# read in the first line as column labels with header=TRUE
 # df2 <-
   # read.csv(file[[2]], header = TRUE) 	# read in the first line as column labels with header=TRUE
 
@@ -46,6 +46,18 @@ names(df_results)[names(df_results) == "What.subject..if.any..did.you.study.or.a
 names(df_results)[names(df_results) == "What.is.the.highest.degree.or.level.of.school.you.have.completed.Â.If.currently.enrolled..highest.degree.received."] <- "highestDegree"
 names(df_results)[names(df_results) == "How.much.experience.do.you.have.with.virtual.reality..VR..and.augmented.reality..AR....VR."] <- "experienceVR"
 names(df_results)[names(df_results) == "How.much.experience.do.you.have.with.virtual.reality..VR..and.augmented.reality..AR....AR."] <- "experienceAR"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...I.felt.tired.before.the.experiment.."] <- "tiredBefore"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...I.felt.tired.after.the.experiment.."] <- "tiredAfter"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...I.felt.comfortable.trying.to.sleep.with.a.VR.head.mounted.display.."] <- "comfortableHeadset"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...I.felt.more.awake.after.solving.the.task.."] <- "awakeAfterSolving"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...The.transition.from.sleeping.resting.to.solving.tasks.was.easy.for.me.."] <- "transitionEasy"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...I.can.imagine.being.woken.up.like.in.the.experiment.to.prepare.for.dangerous.situations..e.g..take.the.control.of.a.car..."] <- "imagineWakingUp"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...I.can.imagine.wearing.a.head.mounted.display.permanently..if.they.become.tiny.and.comfortable.."] <- "permanentWearing"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...I.usually.sleep.with.a.sleep.mask.on.."] <- "sleepingMask"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...I.have.experience.with.meditation.."] <- "meditation"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...I.didn.t.feel.that.comfortable.trying.to.sleep.with.a.VR.head.mounted.display.."] <- "notComfortableHeadset"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...I.felt.awake.before.the.experiment.."] <- "awakeBefore"
+names(df_results)[names(df_results) == "To.what.extent.do.you.agree.with.the.following.statements...The.transition.from.sleeping.resting.to.solving.tasks.was.hard.for.me.."] <- "transitionHard"
 
 # Change data selection
 df_results$gender <- as.character(df_results$gender)
@@ -69,6 +81,21 @@ df_results$experienceAR[df_results$experienceAR == "A4"] <- "[A4] Some experienc
 df_results$experienceAR[df_results$experienceAR == "A5"] <- "[A5] More than average experience"
 df_results$experienceAR[df_results$experienceAR == "A6"] <- "[A6] Experienced"
 df_results$experienceAR[df_results$experienceAR == "A7"] <- "[A7] Very highly experienced"
+
+for (i in c("tiredBefore", "tiredAfter", "comfortableHeadset", "awakeAfterSolving", "transitionEasy", "imagineWakingUp", "permanentWearing", "sleepingMask", "meditation", "notComfortableHeadset", "awakeBefore", "transitionHard")) {
+  tmp <- df_results[i]
+  # print(a[a == "A1"])
+  tmp[tmp == "A1"] <- "[A1] Strongly Agree"
+  tmp[tmp == "A2"] <- "[A2] Agree"
+  tmp[tmp == "A3"] <- "[A3] Somewhat agree"
+  tmp[tmp == "A4"] <- "[A4] Neither agree nor disagree"
+  tmp[tmp == "A5"] <- "[A5] Somewhat disagree"
+  tmp[tmp == "A6"] <- "[A6] Disagree"
+  tmp[tmp == "A7"] <- "[A7] Strongly disagree"
+  df_results[i] <- tmp
+}
+rm(i)
+rm(tmp)
 
 # print to console the results of ls() call:
 print("Data objects:")
@@ -186,21 +213,6 @@ if (plot) {
   pie + scale_fill_manual(values = c("#d73027", "#fc8d59", "#fee090", "#ffffbf", "#e0f3f8", "#91bfdb", "#4575b4"))
   ggsave("expAr.pdf", pie, NULL, NULL, 1, 7, 5)
 
-  # # "Collection Count", "Sam Pleasure", "Sam Arousal", "Sam Dominance"
-  # df_plot <-
-  #   data.frame("var" = df_results$approachHaltedTimeAvg,
-  #              "group" = df_results$soundGroup)
-  # df_plot$group <-
-  #   factor(df_plot$group, c("LVHA", "HVHA", "HVLA"))
-  # # Change  automatically color by groups
-  # bp <-
-  #   ggplot(df_plot, aes(x = group, y = var, fill = group)) +
-  #   geom_boxplot() +
-  #   labs(x = "Sound Group", y = "Seconds Stopped") +
-  #   guides(fill = guide_legend(title = "Sound Group"))
-  # bp + scale_fill_manual(values = c("#941c20", "#4A5745", "#009269"))
-  # # bp + theme_classic()
-
   ## Plot Age
   df_plot <- data.frame("var" = df_results$age,
                         "group" = "Player")
@@ -214,7 +226,22 @@ if (plot) {
   bp + scale_fill_manual(values = c("#009269")) + coord_flip()
   ggsave("age.pdf", bp, NULL, NULL, 1, 7, 5)
   
+
+  for (i in c("tiredBefore", "tiredAfter", "comfortableHeadset", "awakeAfterSolving", "transitionEasy", "imagineWakingUp", "permanentWearing", "sleepingMask", "meditation", "notComfortableHeadset", "awakeBefore", "transitionHard")) {
+    t <- table(df_results[i])
+    g <- c("[A1] Strongly Agree", "[A2] Agree", "[A3] Somewhat agree", "[A4] Neither agree nor disagree", "[A5] Somewhat disagree", "[A6] Disagree", "[A7] Strongly disagree")
+    v <- c(t[g[1]], t[g[2]], t[g[3]], t[g[4]], t[g[5]], t[g[6]], t[g[7]])
+    v[is.na(v)] <- 0
+    v <- as.numeric(as.character(v))
+    df_plot <- data.frame(group = g, value = v)
+    p<-ggplot(data=df_plot, aes(x=group, y=value)) +
+      geom_bar(stat="identity")
+    ggsave(paste(i, ".pdf", sep=""), p, NULL, NULL, 1, 7, 5)
+  }
+  rm(i)
+
   rm(df_plot)
   rm(bp)
   rm(pie)
+  rm(p)
 }
